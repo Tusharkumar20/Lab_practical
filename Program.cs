3632d7956b487a9df5347af84c2879ba74e5a14c
Tusharkumar20 <tusharkumar20122003@gmail.com>
2026-05-04 09:00:05 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("Enter your name:");
+          string name = Console.ReadLine();
+          Console.WriteLine("Hello, " + name + "!");
+          Console.WriteLine("select your plan : \n1. Basic(1000)\n2. Premium(2000) 10% off\n3. Pro(3000) 20% off");
+           Console.WriteLine("Enter the number corresponding to your plan:");
+            int plan = int.Parse(Console.ReadLine());
+           for (int i = 0; i < 3; i++)
+           {
+               if (plan == 1)
+               {
+                   Console.WriteLine("You have selected the Basic plan (1000). ");
+                   break;
+               }
+               else if (plan == 2)
+               {
+                   Console.WriteLine("You have selected the Premium plan (2000). In this plan you get 10% off");
+                   break;
+               }
+               else if (plan == 3)
+               {
+                   Console.WriteLine("You have selected the Pro plan(3000).In this plan you get 20% off ");
+                   break;
+               }
+               else
+               {
+                   Console.WriteLine("Invalid selection. Please try again.");
+                   plan = int.Parse(Console.ReadLine());
+               }
+           }
+           Console.WriteLine("please select the usage type : \n1. Personal\n2. Business");
+           int usageType = int.Parse(Console.ReadLine());
+           for (int i = 0; i < 3; i++)
+           {
+               if (usageType == 1)
+               {
+                   Console.WriteLine("You have selected Personal usage.");
+                   break;
+               }
+               else if (usageType == 2)
+               {
+                   Console.WriteLine("You have selected Business usage.");
+                   break;
+               }
+               else
+               {
+                   Console.WriteLine("Invalid selection. Please try again.");
+                   usageType = int.Parse(Console.ReadLine());
+               }
+           }
+        }
+    }
+}
